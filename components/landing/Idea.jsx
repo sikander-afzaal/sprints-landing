50e0128ffeb0bcb19b35be6c6997f5df1d9838c5
Shayan Ali <64377310+shayanali4@users.noreply.github.com>
2023-04-07 20:19:32 +0500

    some fixes

--- components/landing/Idea.jsx
+++ components/landing/Idea.jsx
@@ -7,7 +7,7 @@ const Idea = () => {
       id="idea"
       className="wrapper  sm:mt-[90px] xl:mt-[150px]"
     >
-      <div className="contain flex-col justify-center items-center gap-[50px] xl:gap-[100px]">
+      <div className="contain flex-col justify-center items-center gap-[50px] xl:gap-[100px] mt-10 px-10">
         <div className="text-center">
           <h2 className="text-[35px] sm:text-[40px] lg:text-[50px]  leading-[1.2] font-black text-white">
             Idea to product
@@ -16,33 +16,32 @@ const Idea = () => {
             Great ideas deserve to become great products
           </p>
         </div>
-        {/* Shaya - White images should be smaller */}
-        {/* Shaya - There is a bug with the first image, it wont reduce in size, when width: 100% is added*/}
-        <div className="flex justify-center xl:justify-between items-center gap-1 w-full">
-
-          <Image src={"/idea-img.png"} width={350} height={340} alt="...." />
-          <div className="xl:block hidden">
-            <Image src={"/idea-img.png"} width={350} height={340} alt="...." />
+        <div className="flex justify-center xl:justify-between items-center gap-1 w-90">
+          <div className="lg:block hidden">
+            <Image style={{ maxWidth: '80%' }} src={"/idea-img.png"} width={350} height={340} alt="...." />
           </div>
-          <div className="xl:block hidden">
-            <Image src={"/idea-img.png"} width={350} height={340} alt="...." />
+          <div className="lg:block hidden">
+            <Image style={{ maxWidth: '80%' }} src={"/idea-img.png"} width={350} height={340} alt="...." />
+          </div>
+          <div className="lg:block hidden">
+            <Image style={{ maxWidth: '80%' }} src={"/idea-img.png"} width={350} height={340} alt="...." />
           </div>
         </div>
-        <div className="flex justify-start items-start flex-col gap-5 ">
-          <p className="text-base sm:text-xl  font-medium text-white">
-            Creating a startup is a journey full of challenges, but we{"'"}re
-            here to guide you every step of the way.
+        <div className="flex justify-start items-start flex-col gap-5 px-10 md:px-20 lg:px-44">
+          <p className="text-base sm:text-lg  font-medium text-white">
+            <strong>Creating a startup</strong> is a journey full of challenges. You have this <strong>amazing idea</strong>, that will change people{"'"}s lives, but you don{"'"}t the first thing about code.
           </p>
-          <p className="text-base sm:text-xl  font-medium text-white">
-            Our team will work with you to understand your users needs so we can
-            build a solid, scalable, and reliable product for you.
+          <p className="text-base sm:text-lg  font-medium text-white">
+            That{"'"}s why we created <strong>Sprints</strong>.
+            <br/>Our team will work with you to understand your <strong>users needs</strong> so we can
+            build a <strong>solid, scalable, and reliable product</strong> for you.
           </p>
-          <p className="text-base sm:text-xl  font-medium text-white">
-            We{"'"}ve seen firsthand the impact that a well-executed MVP can
+          <p className="text-base sm:text-lg  font-medium text-white">
+            We{"'"}ve seen firsthand the impact that a <strong>well-executed MVP</strong> can
             have on a business, and our team is committed to helping you achieve
-            that same success.
+            that <strong>same success.</strong>
           </p>
-          <p className="text-base sm:text-xl  font-medium text-white">
+          <p className="text-base sm:text-lg  font-medium text-white">
             Here is how….
           </p>
         </div>
